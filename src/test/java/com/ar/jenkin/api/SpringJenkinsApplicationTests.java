9b9c5b1a1e80a63ddeb2290b8e83cc99ae6ea7ab
Rahman1712 <abdulrahmankm1712@gmail.com>
2021-11-21 00:48:09 +0530

    fail commit for pipeline

--- src/test/java/com/ar/jenkin/api/SpringJenkinsApplicationTests.java
+++ src/test/java/com/ar/jenkin/api/SpringJenkinsApplicationTests.java
@@ -13,7 +13,7 @@ class SpringJenkinsApplicationTests {
 	public static Logger logger  = LoggerFactory.getLogger(SpringJenkinsApplicationTests.class);
 	
 	@Test
-	public void contextLoads() {
+	public voids contextLoads() {
 		logger.info("Test case  executing....");
 		logger.info("Test case  executing second log statement....");
 		logger.info("Test case  executing Adding for pipeline checking....");
